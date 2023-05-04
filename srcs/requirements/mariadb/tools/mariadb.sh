c2a275a89e4420e513697e9fe27f6a4635cfbec3
facolomb <facolomb@student.42lausanne.ch>
2023-05-04 15:02:32 +0200

    test

---
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+
+        chown -R mysql:mysql /var/lib/mysql
+
+        mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
+
+        tfile=`mktemp`
+        if [ ! -f "$tfile" ]; then
+                return 1
+        fi
+fi
+if [ ! -d "/var/lib/mysql/mariaDB" ]; then
+
+        cat << EOF > $tfile
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM     mysql.user WHERE User='';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+CREATE DATABASE $DATABASE_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$MYSQL_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+	/usr/bin/mysqld --user=mysql --bootstrap < $tfile
+	rm -f $tfile
+fi
+
+sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
+
+exec /usr/bin/mysqld --user=mysql
--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,20 +1,31 @@
 #!/bin/sh
+sleep 10
 
-if [ -f ./wp-config.php]
-then
-  echo "wordpress already downloaded"
-else
-  wget http://wordpress.org/latest.tar.gz
-  tar xfz latest.tar.gz
-  mv wordpress/* .
-  rm -rf latest.tar.gz
-  rm -rf wordpress
+if [ ! -f /var/www/html/wp-config.php ] ; then
+{
+	wp core download --allow-root
 
-  sed -i "s/username_here/test/g" wp-config-sample.php
-  sed -i "s/password_here/test/g" wp-config-sample.php
-  sed -i "s/localhost/mariadb/g" wp-config-sample.php
-  sed -i "s/database_name_here/test/g" wp-config-sample.php
-  cp wp-config-sample.php wp-config.php
+	mv wordpress/* /var/www/html/
+	rm -rf wordpress/ wordpress-6.0-fr_FR.tar.gz
+
+	wp config create	--allow-root \
+						--dbname=$DATABASE_NAME \
+						--dbuser=$MYSQL_USER \
+						--dbpass=$MYSQL_PASSWORD \
+						--dbhost=$DB_HOST \
+						--path='/var/www/html/' >> log.txt
+
+	wp core install		--url="tchappui.42.fr" \
+						--title="ft_containers" \
+						--admin_user=$MYSQL_USER \
+						--admin_password=$MYSQL_PASSWORD \
+						--admin_email=test@gmail.com \
+						--allow-root --path='/var/www/html/' >> log.txt
+
+	wp user create		--allow-root --role=author \
+						$USER2_LOGIN $USER2_MAIL --user_pass=$USER2_PASS \
+						--path='/var/www/html/' >> /log.txt
+}
 fi
 
-exec "$@"
+./usr/sbin/php-fpm8 -F;
